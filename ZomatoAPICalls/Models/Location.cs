2a68f81ffb55d0c3ae3ddb75fe460357f6754d54
Abhilash Agarwal <abhilash.agarwal.nitsk@gmail.com>
2020-06-02 11:00:30 +0530

    Initial creation of application

---
+++ ZomatoAPICalls/Models/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ZomatoAPICalls.Models
+{
+    public class Location
+    {
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("locality_verbose")]
+        public string LocalityVerbose { get; set; }
+    }
+}
---
+++ ZomatoAPICalls/Models/RestaurantRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ZomatoAPICalls.Models
+{
+    public class RestaurantRecord
+    {
+        [JsonProperty("restaurant")]
+        public Restaurant Restaurant { get; set; }
+    }
+    
+}
+    
