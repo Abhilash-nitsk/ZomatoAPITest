3aea3bd2d2c2947d496187d76f886e8f87b3d05f
Abhilash Agarwal <aagarwal@microsoft.com>
2020-06-02 13:42:18 +0530

    Added the GetReviews option in the application

---
+++ ZomatoAPICalls/Models/ReviewList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ZomatoAPICall.Models
+{
+    public class ReviewList
+    {
+        [JsonProperty("reviews_count")]
+        public int ReviewCount { get; set; }
+
+        [JsonProperty("reviews_shown")]
+        public int ReviewShown { get; set; }
+
+        [JsonProperty("user_reviews")]
+        public ReviewRecord[] Reviews{ get; set; }
+
+    }
+}
---
+++ ZomatoAPICalls/Service/Reviews.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZomatoAPICall.Models;
+using ZomatoAPICalls.Models;
+
+namespace ZomatoAPICalls.Service
+{
+    class Reviews
+    {
+        public static void ShowReviews()
+        {
+            var response = ZomatoInfo.GetReviews();
+
+            var reviews = response.Reviews;
+
+            Console.WriteLine($"Reviews found : {response.ReviewCount}");
+            Console.WriteLine($"Reviews shown : {response.ReviewShown}");
+            Console.WriteLine();
+
+            Console.WriteLine($"{"Rating",-10} {"Rating Age",-50} {"Name",-60}");
+            foreach (var r in reviews)
+            {
+                var review = r.Review;
+                Console.WriteLine($"{review.Rating,-10} {review.RatingAge,-50} {review.Visitor.Name,-60}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
